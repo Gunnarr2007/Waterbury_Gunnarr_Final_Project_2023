47714c60998c8748e8aeb65970d9a6ec0e587e91
Gunnarr2007 <142431185+Gunnarr2007@users.noreply.github.com>
2023-12-01 12:38:44 -0800

    Add files via upload
    Final Project for Introduction to computer science.

---
+++ Final/main.py
@@ -0,0 +1,24 @@
+# This file was created by Gunnarr Waterbury
+
+'''
+Goals:
+make a shooter that is multiplayer maybe vs or a co-op
+add respawning mobs and new areas
+make the player try and get something 
+add dif weapons
+
+will use:
+
+randint 
+math 
+pygame or other screem 
+and other most likely 
+
+Source:
+https://www.techwithtim.net/tutorials/python-online-game-tutorial
+https://medium.com/@DakaiZhou/generate-random-location-coordinates-within-given-area-with-python-1d6e62b6e382
+'''
+
+import random
+from random import randint
+import os 
